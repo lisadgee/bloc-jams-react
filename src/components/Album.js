0826f2f3fb8c82dfb735af5c6722f964234411f8
lisa gee <lisadgee@hotmail.com>
2018-05-18 20:46:08 -0400

    updated master

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,7 +6,7 @@ class Album extends Component {
   constructor(props) {
     super(props);
 
-    const album = albumData.find( album => {
+    const album = albumData.find(album => {
       return album.slug === this.props.match.params.slug
     });
 
@@ -19,22 +19,25 @@ class Album extends Component {
     return (
       <section className="album">
         <section id="album-info">
-        <img id="album-cover-art" src={this.state.album.albumCover} alt="album" />
-           <div className="album-details">
-           <h1 id="album-title">{this.state.album.title}</h1>
-             <h2 className="artist">{this.state.album.artist}</h2>
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-         </section>  
-         <table id="song-list">
-           <colgroup>
-             <col id="song-number-column" />
-             <col id="song-title-column" />
-             <col id="song-duration-column" />
-           </colgroup>  
-           <tbody>
-           </tbody>
-         </table>
+          <img id="album-cover-art" src={this.state.album.albumCover} alt="album" />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+            {
+              this.state.album.songs.map((song, index) => <tr><td></td>{index + 1}<td>{song.title}</td><td>{song.duration}</td></tr>)
+            }
+          </tbody>
+        </table>
       </section>
     );
   }
